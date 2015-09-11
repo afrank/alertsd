cb047b90cd7e8bf6ecd702e76584953fa43432f0
afrank <afrank@newrelic.com>
2015-09-10 22:06:25 -0700

    adding user

---
+++ alertsd/api/user.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+#from django.shortcuts import render_to_response
+from alertsd.models import *
+from restless.views import Endpoint
+
+from restless.dj import DjangoResource
+from restless.preparers import FieldsPreparer
+
+from restless.exceptions import BadRequest
+
+class UserResource(DjangoResource):
+    preparer = FieldsPreparer(fields={
+        'id': 'id',
+        'api_key': 'api_key',
+    })
+    def list(self):
+        return User.objects.all()
+
+    def detail(self, pk):
+        return User.objects.get(id=pk)
+
+    def is_authenticated(self):
+        return True
+
+    def create(self):
+        return User.objects.create(
+            api_key=self.data['api_key']
+        )
+
+    def update(self, pk):
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            user = User()
+
+        if 'api_key' in self.data:
+            user.api_key = self.data['api_key']
+        user.save()
+        return user
+
+    def delete(self, pk):
+        try:
+            user = User.objects.get(id=pk)
+        except User.DoesNotExist:
+            raise BadRequest("User not found.")
+        user.delete()
+
--- urls.py
+++ urls.py
@@ -7,7 +7,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 from alertsd.api.default import *
+from alertsd.api.user import *
 
 urlpatterns = patterns('',
     url(r'^$', Healthcheck.as_view(), name='Default'),
+    url(r'^api/user/$', UserResource.as_list(), name='api_user_list'),
+    url(r'^api/user/(?P<pk>\d+)/$', UserResource.as_detail(), name='api_user_detail'),
 )
