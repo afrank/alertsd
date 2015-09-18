6e4d08f810c25b365a07b052dd80f277d1c2195a
afrank <adam@antilogo.org>
2015-09-18 02:02:42 +0000

    updating install script

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+apt-get install rabbitmq-server celeryd python-django gunicorn supervisor nginx python-pip
+pip install DjangoRestless
+
+python manage.php syncdb
+
 rsync -av --exclude=.git --exclude="*.pyc" ./ /opt/alertsd/
 [[ -d /opt/alertsd/run ]] || mkdir /opt/alertsd/run
 chown -R www-data. /opt/alertsd/
+
 cp conf/nginx/alertsd.conf /etc/nginx/conf.d/
 cp conf/supervisor/alertsd.conf /etc/supervisor/conf.d/
 cp conf/celeryd/celeryd /etc/default/
-
-# TODO do pip/apt-get stuff here
 
 service supervisor restart
 service celeryd restart
